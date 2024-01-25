a9b4700d6e376b2b998d4b1cc39f92cca397961a
Zachary Semaan <semaanza@gmail.com>
2024-01-25 09:09:45 -0500

    Tina config reinstall

--- tina/config.ts
+++ tina/config.ts
@@ -1,50 +1,51 @@
-import { defineConfig } from "tinacms"
+import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
-	process.env.GITHUB_BRANCH ||
-	process.env.VERCEL_GIT_COMMIT_REF ||
-	process.env.HEAD ||
-	"master"
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
 
 export default defineConfig({
-	branch,
-	clientId: process.env.TINA_CLIENT_ID,
-	token: process.env.TINA_TOKEN,
+  branch,
 
-	build: {
-		outputFolder: "admin",
-		publicFolder: "public",
-	},
-	media: {
-		tina: {
-			mediaRoot: "",
-			publicFolder: "public",
-		},
-	},
-	// See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
-	schema: {
-		collections: [
-			{
-				name: "post",
-				label: "Posts",
-				path: "content/posts",
-				fields: [
-					{
-						type: "string",
-						name: "title",
-						label: "Title",
-						isTitle: true,
-						required: true,
-					},
-					{
-						type: "rich-text",
-						name: "body",
-						label: "Body",
-						isBody: true,
-					},
-				],
-			},
-		],
-	},
-})
+  clientId: process.env.TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});

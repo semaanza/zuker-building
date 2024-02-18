76de5f0d9e9aa156a7dd25d56341945102021b6e
zach <zach@netx.net>
2024-02-17 20:38:01 -0500

    Contact initial + other updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}"],
+	theme: {
+		extend: {
+			backgroundImage: (theme) => ({
+				"hero-pattern": "url('')",
+			}),
+		},
+	},
+	plugins: [],
+}
